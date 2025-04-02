f98cbcdc3212dea71a8b17038b5504c7ecc81680
ryupatterson <54785861+ryupatterson@users.noreply.github.com>
2025-04-02 10:52:49 -0400

    Update toolprint.py
    Backward compat changes

--- lib/toolprint.py
+++ lib/toolprint.py
@@ -7,13 +7,13 @@ ERR_PREFIX     = "[-]"
 SUCCESS_PREFIX = "[+]"
 
 def print_info(message: str):
-    print(f"{format_string(INFO_PREFIX, foreground="blue")} {message}")
+    print(f"{format_string(INFO_PREFIX, foreground='blue')} {message}")
 
 def print_warn(message: str):
-    print(f"{format_string(WARN_PREFIX, foreground="yellow")} {message}")
+    print(f"{format_string(WARN_PREFIX, foreground='yellow')} {message}")
 
 def print_err(message: str):
-    print(f"{format_string(ERR_PREFIX, foreground="red")} {message}")
+    print(f"{format_string(ERR_PREFIX, foreground='red')} {message}")
 
 def print_success(message: str):
-    print(f"{format_string(SUCCESS_PREFIX, foreground="green")} {message}")
+    print(f"{format_string(SUCCESS_PREFIX, foreground='green')} {message}")
